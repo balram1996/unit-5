6686da685ad03c19e3d06d655c6f205932460913
Balram <kurmibalram936@gmail.com>
2021-10-16 03:07:24 +0530

    adding routes

--- week-1/react-redux-1/assignment/redux-todo/src/App.js
+++ week-1/react-redux-1/assignment/redux-todo/src/App.js
@@ -1,9 +1,29 @@
 import react from "react";
-import Todo from "./components/Todo"
+import Todo from "./components/Todo";
+import Test from "./components/test"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <>
-    <Todo/>
+    <Router>
+      <Switch>
+      <Route exact path="/">
+          <Todo/>
+      </Route>
+
+      <Route exact path="/test">
+          <Test/>
+      </Route>
+
+      </Switch>
+    </Router>
+    
     </>
   );
 }

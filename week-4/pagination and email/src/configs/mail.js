0f38339a602c771426d01f64b5969f63b47c49be
Balram <kurmibalram936@gmail.com>
2021-10-27 00:28:02 +0530

    refactor second with .env files

--- week-4/pagination and email/src/configs/mail.js
+++ week-4/pagination and email/src/configs/mail.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer")
 //sending email logic--
 module.exports = nodemailer.createTransport({
@@ -5,7 +6,7 @@ module.exports = nodemailer.createTransport({
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "82df66f88cad56", // generated ethereal user
-      pass: "c29c41b9b4763e", // generated ethereal password
+      user: process.env.SMTP_USERNAME, // generated ethereal user
+      pass: process.env.SMTP_PASSWORD, // generated ethereal password
     },
   });
